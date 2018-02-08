457dd175f207aa8373b6a2bfc50780bfb73c25d4
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-08 01:24:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   
-  var keys = [];
+  let index = 0;
   
-  document.body.addEventListener('keydown', function() {
+  document.body.addEventListener('keydown', function(e) {
     
     const key = parseInt(e.detail || e.which);
     
